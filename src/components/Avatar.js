a361c7b7e2be2417dd4a94334abb658fd29cc22f
Leon Machens <leon.machens@googlemail.com>
2019-12-10 10:53:05 +0000

    Add Avatar (#2)

---
+++ src/components/Avatar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Img = styled.img`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  background: #e0e4e3;
+`;
+
+function Avatar({
+  src = "https://www.kirkleescollege.ac.uk/wp-content/uploads/2015/09/default-avatar.png"
+}) {
+  return <Img src={src} />;
+}
+
+export default Avatar;
