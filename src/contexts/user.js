8f2ac0fe82b33fb2fa8c82f7165dfbb9101d42b2
lmachens <leon.machens@googlemail.com>
2019-12-11 11:38:55 +0100

    Add custom user hooks

--- src/contexts/user.js
+++ src/contexts/user.js
@@ -2,7 +2,35 @@ import React from "react";
 
 export const UserContext = React.createContext(null);
 
-export const useUser = function() {
+export function useUser() {
   const { user } = React.useContext(UserContext);
   return user;
-};
+}
+
+export function useAccountMethods() {
+  const { login, logout } = React.useContext(UserContext);
+  return { login, logout };
+}
+
+export function UserProvider({ children }) {
+  const [user, setUser] = React.useState(null);
+
+  function logout() {
+    setUser(null);
+  }
+
+  function login() {
+    setUser({
+      name: "Call me Ape",
+      avatarImgSrc:
+        "http://www.freakingnews.com/pictures/78500/Monkey-Business--78549.jpg"
+    });
+  }
+
+  const value = {
+    user,
+    logout,
+    login
+  };
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+}
