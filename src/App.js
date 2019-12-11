9a9e3b08886b87bc3c824f457cc937fd992b82d3
Leon Machens <leon.machens@googlemail.com>
2019-12-11 11:27:07 +0000

    Add useFetch hook (#8)

--- src/App.js
+++ src/App.js
@@ -3,16 +3,35 @@ import "./App.css";
 import { UserProvider } from "./contexts/user";
 import UserAvatar from "./components/UserAvatar";
 import AccountButtons from "./components/AccountButtons";
+import useFetch from "./hooks/useFetch";
 
 function App() {
+  const [url, setUrl] = React.useState(
+    "https://pokeapi.codasds/api/v2/pokemon"
+  );
+  const [{ data, loading, error }, retry] = useFetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
   return (
     <UserProvider>
       <div className="App">
         <header className="App-header">
           <UserAvatar />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <button onClick={() => setUrl("https://pokeapi.co/api/v2/pokemon")}>
+              Change Fetch
+            </button>
           </p>
+          <p>
+            {data && <span>Pokemons {data.count}</span>}
+            {loading && <span>Loading...</span>}
+            {error && <span>!!!ERROR!!!</span>}
+          </p>
+          {error && <button onClick={() => retry()}>Retry</button>}
           <AccountButtons />
         </header>
       </div>
